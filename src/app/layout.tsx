6dfed0dbdd358728f4b441fdd048f697ae7b5f5d
Thiago Henrique Campos Fidêncio <thiagohcfidencio@gmail.com>
2025-03-10 20:02:39 -0300

    :sparkles: feat: add navbar to main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/contexts/theme-provider";
+import Navbar from "@/components/template/Navbar";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -25,9 +27,17 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				className={`${geistSans.variable} ${geistMono.variable} w-2/3 mx-auto antialiased`}
 			>
-				{children}
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<Navbar />
+					{children}
+				</ThemeProvider>
 			</body>
 		</html>
 	);
