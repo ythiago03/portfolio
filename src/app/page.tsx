17c7cc1d5eace6fe4ea6f9abf31083978bce92f2
Thiago Henrique Campos Fidêncio <thiagohcfidencio@gmail.com>
2025-03-13 19:25:49 -0300

    :sparkles: feat: create social grid icons and stack list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,85 @@
-import { Github } from "lucide-react";
+import NextjsIcon from "@/components/common/NextjsIcon";
+import { Codepen, Github, Linkedin } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
+interface Stack {
+	id: string;
+	imgPath: string;
+}
+
 export default function Home() {
+	const stacks: Stack[] = [
+		{
+			id: "angular",
+			imgPath: "/assets/angular.png",
+		},
+		{
+			id: "azure",
+			imgPath: "/assets/azure.png",
+		},
+		{
+			id: "css",
+			imgPath: "/assets/css.png",
+		},
+		{
+			id: "docker",
+			imgPath: "/assets/docker.png",
+		},
+		{
+			id: "figma",
+			imgPath: "/assets/figma.png",
+		},
+		{
+			id: "firebase",
+			imgPath: "/assets/firebase.png",
+		},
+		{
+			id: "html",
+			imgPath: "/assets/html.png",
+		},
+		{
+			id: "ionic",
+			imgPath: "/assets/ionic.png",
+		},
+		{
+			id: "java",
+			imgPath: "/assets/java.png",
+		},
+		{
+			id: "javascript",
+			imgPath: "/assets/javascript.png",
+		},
+		{
+			id: "mysql",
+			imgPath: "/assets/mysql.png",
+		},
+		{
+			id: "nodejs",
+			imgPath: "/assets/nodejs.png",
+		},
+
+		{
+			id: "react",
+			imgPath: "/assets/react.png",
+		},
+		{
+			id: "scss",
+			imgPath: "/assets/scss.png",
+		},
+		{
+			id: "spring",
+			imgPath: "/assets/spring.png",
+		},
+		{
+			id: "tailwind",
+			imgPath: "/assets/tailwind.png",
+		},
+		{
+			id: "typescript",
+			imgPath: "/assets/typescript.png",
+		},
+	];
 	return (
 		<>
 			<main>
@@ -19,7 +96,7 @@ export default function Home() {
 								alt="Serial Experiments Lain gif"
 								width={0}
 								height={0}
-								className="size-7/12"
+								className="size-10/12"
 								draggable={false}
 							/>
 						</div>
@@ -90,13 +167,69 @@ export default function Home() {
 								height={0}
 								className="size-6"
 							/>
-							<span className="absolute bottom-15 -left-11 whitespace-nowrap  overflow-hidden w-2/3  text-xl font-bold -rotate-90">
-								A Desire to disappear
-							</span>
-							<span className="absolute -bottom-10 -right-10 block size-48 animate-spin duration-10000 rounded-full overflow-hidden border bg-[url('/assets/song-cover.jpg')] bg-cover bg-center shadow-[0px_0px_60px_0px_rgba(247,_250,_252,_0.4)]">
+							<div className="absolute bottom-14 -left-11 overflow-hidden w-2/3 -rotate-90">
+								<div className="marquee space-x-8">
+									<span className="whitespace-nowrap text-xl font-bold">
+										A Desire to disappear
+									</span>
+								</div>
+							</div>
+							<span className="absolute -bottom-10 -right-10 block size-48 animate-spin duration-50000 rounded-full overflow-hidden border bg-[url('/assets/song-cover.jpg')] bg-cover bg-center shadow-[0px_0px_60px_0px_rgba(247,_250,_252,_0.4)]">
 								<span className="absolute block top-0 right-0 bottom-0 left-0 m-auto rounded-full size-8 border-2 border-gray bg-black" />
 							</span>
 						</Link>
+					</div>
+
+					<div className="col-span-3 h-48 w-full  rounded-lg overflow-hidden">
+						<div className="w-full h-full grid grid-cols-2 gap-3">
+							<Link
+								href={"https://www.linkedin.com/in/ythiago03/"}
+								target="_blank"
+								className="w-full h-full p-4 flex items-center justify-center text-xl border  rounded-lg bg-blue-500 hover:scale-[0.99] duration-300 ease-out hover:ease-in"
+							>
+								<Linkedin className="size-10 text-white" />
+							</Link>
+							<Link
+								href={"https://codepen.io/Ythiago03"}
+								target="_blank"
+								className="w-full h-full p-4 flex items-center justify-center text-xl border  rounded-lg bg-white hover:scale-[0.99] duration-300 ease-out hover:ease-in"
+							>
+								<Codepen className="size-10 text-black" />
+							</Link>
+							<div className="relative col-span-2 w-full h-full flex items-center ">
+								<span className="absolute left-0 z-10 block bg-gradient-to-r from-background to- h-full w-32" />
+								<div className="flex w-full items-center gap-3 marquee ">
+									<NextjsIcon />
+									{stacks.map((stack: Stack) => (
+										<img
+											key={stack.id}
+											src={stack.imgPath}
+											alt="Youtube Music Logo"
+											width={0}
+											height={0}
+											className="w-12"
+										/>
+									))}
+									<NextjsIcon />
+									{stacks.map((stack: Stack) => (
+										<img
+											key={stack.id}
+											src={stack.imgPath}
+											alt="Youtube Music Logo"
+											width={0}
+											height={0}
+											className="w-12"
+										/>
+									))}
+								</div>
+								<span className="absolute right-0 z-16 block bg-gradient-to-l from-background to- h-full w-32" />
+							</div>
+						</div>
+					</div>
+					<div className="col-span-2 h-48 w-full flex justify-center items-center rounded-lg overflow-hidden border  hover:scale-[0.99] duration-300 ease-out hover:ease-in">
+						<h3 className="text-2xl -rotate-10 font-semibold">
+							Blog (Under Construction)
+						</h3>
 					</div>
 				</section>
 			</main>
