791bde922574a2332343fbfab7f4d88b6a4574fc
Thiago Henrique Campos Fidêncio <thiagohcfidencio@gmail.com>
2025-03-14 17:47:34 -0300

    :sparkles: feat (Footer.tsx): create footer component

---
+++ src/components/template/Footer.tsx
@@ -0,0 +1,32 @@
+import { ExternalLink } from "lucide-react";
+import Link from "next/link";
+
+const Footer = () => {
+	return (
+		<footer className="flex justify-between border-t py-10">
+			<Link
+				href={"https://github.com/ythiago03/portfolio"}
+				target="_blank"
+				className="flex font-semibold text-lg items-center gap-1 hover:text-blue-400"
+			>
+				<ExternalLink />
+				Github
+			</Link>
+			<p>
+				Built with <span className="font-bold">Next.js</span>,{" "}
+				<span className="font-bold">TailwindCss</span> and{" "}
+				<span className="font-bold">Shadcn</span> by{" "}
+				<Link
+					href={"https://github.com/ythiago03"}
+					target="_blank"
+					className="font-bold hover:text-blue-400"
+				>
+					Thiago Fidêncio
+				</Link>
+				.
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
