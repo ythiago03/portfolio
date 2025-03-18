e56f5d3cdd96b6805f29f6d727cfdedb82bb0c20
Thiago Henrique Campos Fidêncio <thiagohcfidencio@gmail.com>
2025-03-18 20:19:58 -0300

    :sparkles: feat (about page.tsx): create expirence, education and contact sections

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,19 @@
 import {
 	Brain,
+	BriefcaseBusiness,
 	Clapperboard,
+	Codepen,
+	ExternalLink,
 	FileDown,
 	Gamepad2,
 	GitGraph,
+	Github,
+	Linkedin,
+	Mail,
+	Phone,
 	Star,
 	Trophy,
+	University,
 	UserPlus,
 } from "lucide-react";
 
@@ -118,15 +126,15 @@ function page() {
 
 	const stacks: Stack[] = [];
 	return (
-		<main className="mt-40">
-			<section className="flex mt-20 gap-3">
+		<main className="mt-30 xl:mt-40">
+			<section className="flex flex-col-reverse xl:flex-row mt-20 gap-3">
 				<div
-					className="group relative w-1/4 h-96 shadow-sm rounded-lg transition-transform duration-300 hover:rotate-x-6 hover:rotate-y-6 hover:scale-105"
+					className="group relative w-full h-96 xl:w-1/4 xl:h-auto  shadow-sm rounded-lg transition-transform duration-300 hover:rotate-x-6 hover:rotate-y-6 hover:scale-105"
 					style={{ perspective: "1000px" }}
 				>
 					<div className="h-full w-full bg-[url('/assets/user-photo.jpg')] bg-cover bg-center rounded-lg shadow-sm transform transition-transform duration-300 group-hover:rotate-x-6 group-hover:rotate-y-6" />
 				</div>
-				<div className="space-y-3 w-3/4">
+				<div className="space-y-3 w-full xl:w-3/4">
 					<h1 className="text-center text-6xl md:text-7xl font-extrabold">
 						About me
 					</h1>
@@ -260,7 +268,7 @@ function page() {
 				</div>
 			</section>
 			<section className="mt-20">
-				<h2 className="flex items-center gap-3 text-2xl font-semibold">
+				<h2 className="sticky top-20 w-full h-20 flex items-center gap-3 text-2xl font-semibold rounded-lg bg-background">
 					<Brain /> Skills
 				</h2>
 				<Accordion type="single" collapsible className="w-full mt-10">
@@ -268,7 +276,7 @@ function page() {
 						<AccordionTrigger className="text-2xl">
 							I use it at work
 						</AccordionTrigger>
-						<AccordionContent className="flex gap-3 flex-wrap">
+						<AccordionContent className="flex gap-3 flex-wrap justify-center md:justify-start">
 							{usedAtWorkStaks.map((stack: Stack) => (
 								<div
 									key={stack.id}
@@ -288,7 +296,7 @@ function page() {
 						<AccordionTrigger className="text-2xl">
 							I have knowledge
 						</AccordionTrigger>
-						<AccordionContent className="flex gap-3 flex-wrap">
+						<AccordionContent className="flex gap-3 flex-wrap justify-center md:justify-start">
 							{haveKnownledgeStaks.map((stack: Stack) => (
 								<div
 									key={stack.id}
@@ -308,7 +316,7 @@ function page() {
 						<AccordionTrigger className="text-2xl">
 							I am studying
 						</AccordionTrigger>
-						<AccordionContent className="flex gap-3 flex-wrap">
+						<AccordionContent className="flex gap-3 flex-wrap justify-center md:justify-start">
 							{learningStaks.map((stack: Stack) => (
 								<div
 									key={stack.id}
@@ -328,7 +336,7 @@ function page() {
 						<AccordionTrigger className="text-2xl">
 							I know a little
 						</AccordionTrigger>
-						<AccordionContent className="flex gap-3 flex-wrap">
+						<AccordionContent className="flex gap-3 flex-wrap justify-center md:justify-start">
 							{otherStaks.map((stack: Stack) => (
 								<div
 									key={stack.id}
@@ -345,6 +353,189 @@ function page() {
 						</AccordionContent>
 					</AccordionItem>
 				</Accordion>
+			</section>
+			<section className="mt-20">
+				<h2 className="sticky top-20 w-full h-20 flex items-center gap-3 text-2xl font-semibold rounded-lg bg-background">
+					<BriefcaseBusiness /> Expirence
+				</h2>
+				<div className="mt-10">
+					<div className="w-full flex flex-col gap-3 p-6 rounded-lg bg-muted/20 shadow-sm">
+						<p>
+							Nov 2024 -
+							<span className="font-semibold text-blue-400"> Present</span>
+							<span className="text-muted-foreground "> 10 months</span>
+						</p>
+
+						<div className="space-y-1 my-3">
+							<p className="text-muted-foreground">Interniship</p>
+							<p className="text-2xl font-semibold">Full-stack developer</p>
+							<Link
+								href={"https://mestria.com.br/"}
+								target="_blank"
+								className="inline-flex gap-1 text-muted-foreground underline hover:text-blue-400"
+							>
+								<ExternalLink className="size-6" />
+								Mestria Automação e Tecnologia
+							</Link>
+						</div>
+						<p>
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+							Cupiditate quae laboriosam id eum ipsum eveniet amet quo expedita,
+							at voluptas atque maiores non nihil molestiae facere, ipsa odio!
+							Molestias, reiciendis?
+						</p>
+					</div>
+				</div>
+				<div className="mt-10">
+					<div className="w-full flex flex-col gap-3 p-6 rounded-lg bg-muted/20 shadow-sm">
+						<p>
+							Mar 2024 - Oct 2024
+							<span className="text-muted-foreground"> 10 months</span>
+						</p>
+
+						<div className="space-y-1 my-3">
+							<p className="text-muted-foreground">Interniship</p>
+							<p className="text-2xl font-semibold">Full-stack developer</p>
+							<Link
+								href={"https://cartaosaudeouro.com.br/"}
+								target="_blank"
+								className="inline-flex gap-1 text-muted-foreground underline hover:text-blue-400"
+							>
+								<ExternalLink className="size-6" />
+								Cartão Saúde Ouro
+							</Link>
+						</div>
+						<p>
+							Developed front-end applications using Angular, ensuring a dynamic
+							and user-friendly experience. Built and integrated Java APIs and
+							microservices, enhancing system architecture. Managed deployments
+							and server configurations using Docker, improving application
+							scalability and deployment efficiency.
+						</p>
+					</div>
+				</div>
+				<div className="mt-10">
+					<div className="w-full flex flex-col gap-3 p-6 rounded-lg bg-muted/20 shadow-sm">
+						<p>
+							Oct 2022 - Jan 2024
+							<span className="text-muted-foreground "> 10 months</span>
+						</p>
+
+						<div className="space-y-1 my-3">
+							<p className="text-muted-foreground">Apprentice</p>
+							<p className="text-2xl font-semibold">Administrative Assistant</p>
+							<Link
+								href={"https://whoid.com/"}
+								target="_blank"
+								className="inline-flex gap-1 text-muted-foreground underline hover:text-blue-400"
+							>
+								<ExternalLink className="size-6" />
+								Who Anti Fraud
+							</Link>
+						</div>
+						<p>
+							Developed company software using TypeScript, implementing the
+							Builder Design Pattern to improve code structure and reusability.
+							Worked with SQL Server, performing efficient database queries and
+							manipulations to optimize system performance. Focused on clean,
+							maintainable, and scalable code solutions.
+						</p>
+					</div>
+				</div>
+			</section>
+			<section className="mt-20">
+				<h2 className="sticky top-20 w-full h-20 flex items-center gap-3 text-2xl font-semibold rounded-lg bg-background">
+					<University /> Education
+				</h2>
+				<div className="mt-10">
+					<div className="w-full flex flex-col gap-3 p-6 rounded-lg bg-muted/20 shadow-sm">
+						<p>
+							Jan 2022 -
+							<span className="text-muted-foreground ">
+								{" "}
+								Dec 2025 10 months
+							</span>
+						</p>
+
+						<div className="space-y-1 my-3">
+							<p className="text-muted-foreground">Bachelor Degree</p>
+							<p className="text-2xl font-semibold">Computer Cience</p>
+							<Link
+								href={"https://descomplica.com.br/faculdade/"}
+								target="_blank"
+								className="inline-flex gap-1 text-muted-foreground underline hover:text-blue-400"
+							>
+								<ExternalLink className="size-6" />
+								Faculdade Descomplica
+							</Link>
+						</div>
+						<p>
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+							Cupiditate quae laboriosam id eum ipsum eveniet amet quo expedita,
+							at voluptas atque maiores non nihil molestiae facere, ipsa odio!
+							Molestias, reiciendis?
+						</p>
+					</div>
+				</div>
+			</section>
+			<section className="mt-20">
+				<h2 className="sticky top-20 w-full h-20 flex items-center gap-3 text-2xl font-semibold rounded-lg bg-background">
+					<Phone /> Contact
+				</h2>
+				<div className="mt-10 grid grid-cols-1 gap-3 md:grid-cols-2">
+					<div className="w-full h-25 flex flex-col gap-3 p-1 justify-center items-center rounded-lg bg-muted/20 shadow-sm">
+						<div className="flex items-center gap-3 text-xl text-muted-foreground">
+							<Mail />
+							Send an email
+						</div>
+						<Link
+							target="_blank"
+							href={"mailto:ythiagohcfidencio@gmail.com"}
+							className="underline hover:text-blue-400"
+						>
+							ythiagohcfidencio@gmail.com
+						</Link>
+					</div>
+					<div className="w-full h-25 flex flex-col gap-3 p-1 justify-center items-center rounded-lg bg-muted/20 shadow-sm">
+						<div className="flex items-center gap-3 text-xl text-muted-foreground">
+							<Linkedin />
+							Conect with me
+						</div>
+						<Link
+							target="_blank"
+							href={"https://www.linkedin.com/in/ythiago03/"}
+							className="underline hover:text-blue-400"
+						>
+							ythiago03
+						</Link>
+					</div>
+					<div className="w-full h-25 flex flex-col gap-3 p-1 justify-center items-center rounded-lg bg-muted/20 shadow-sm">
+						<div className="flex items-center gap-3 text-xl text-muted-foreground">
+							<Github />
+							View my projects
+						</div>
+						<Link
+							target="_blank"
+							href={"https://github.com/ythiago03"}
+							className="underline hover:text-blue-400"
+						>
+							ythiago03
+						</Link>
+					</div>
+					<div className="w-full h-25 flex flex-col gap-3 p-1 justify-center items-center rounded-lg bg-muted/20 shadow-sm">
+						<div className="flex items-center gap-3 text-xl text-muted-foreground">
+							<Codepen />
+							View my pens
+						</div>
+						<Link
+							target="_blank"
+							href={"https://codepen.io/Ythiago03"}
+							className="underline hover:text-blue-400"
+						>
+							ythiago03
+						</Link>
+					</div>
+				</div>
 			</section>
 		</main>
 	);
