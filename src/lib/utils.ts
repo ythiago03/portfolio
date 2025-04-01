97cd86dc93d9be56f164581e47f6bb9806451c19
Thiago Fidêncio <thiagohcfidencio@gmail.com>
2025-04-01 18:31:26 -0300

    :sparkles: feat: create detailed post page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,23 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export function formatDate(input: string | number): string {
+	const date = new Date(input);
+	return date.toLocaleDateString("en-US", {
+		month: "long",
+		day: "numeric",
+		year: "numeric",
+	});
+}
+
+export function sortPosts(posts: any[]) {
+	return posts.sort((a, b) => {
+		if (a.data > b.data) return -1;
+		if (a.data < b.data) return 1;
+		return 0;
+	});
 }
