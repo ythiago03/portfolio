152f1ddbd4f7cc8f64509b9c598d1bddfcf37cee
Thiago Fidêncio <thiagohcfidencio@gmail.com>
2025-04-01 18:29:12 -0300

    :sparkels: feat (mdx-components.tsx): create component to manipulate mdx components

---
+++ src/components/common/mdx-components.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import * as runtime from "react/jsx-runtime";
+
+const useMDXComponents = (code: string) => {
+	const fn = new Function(code);
+	return fn({ ...runtime }).default;
+};
+
+const components = {
+	Image,
+};
+
+interface MdxProps {
+	code: string;
+}
+
+export function MDXContent({ code }: MdxProps) {
+	const Component = useMDXComponents(code);
+	return <Component components={components} />;
+}
